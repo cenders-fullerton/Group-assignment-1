ee2732f18951ea211f2afeadaffcedf2311a103d
Caleb Enders <cenders@csu.fullerton.edu>
2024-09-23 16:54:19 -0700

    added in the files to add in the programs for the shell.

---
+++ Assignment1/programs.cpp
@@ -0,0 +1,35 @@
+#include "programs.hpp"
+
+void dir(){
+
+}
+
+void help(){
+	
+	}
+
+void vol(){
+	
+	}	
+void path() {
+	
+	}
+
+void tasklist(){
+	
+	}
+
+void notepad(){
+	
+	}
+void echo(){
+	
+	}
+
+void color(){
+	
+	}
+	
+void ping(){
+	
+	}
---
+++ Assignment1/programs.hpp
@@ -0,0 +1,14 @@
+#ifndef PROGRAMS_HPP
+#define PROGRAMS_HPP
+
+void dir();
+void help();
+void vol();
+void path();
+void tasklist();
+void notepad();
+void echo();
+void color();
+void ping();
+
+#endif
