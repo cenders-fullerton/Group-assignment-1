edc28f619c4862d9d69acf3b0d1042d212bbed94
edwintong78 <130247675+edwintong78@users.noreply.github.com>
2024-09-25 01:11:51 -0700

    Update programs.cpp
    I made basic changes to the dir function

--- programs.cpp
+++ programs.cpp
@@ -1,6 +1,44 @@
 #include "programs.hpp"
 
 void dir(){
+    struct dirent *d;
+    struct stat dst;
+
+    DIR *dr;
+
+    string path = ".\\";
+
+    if (dr != NULL) {
+
+        for (d = readdir(dr); d != NULL; d = readdir(dr)) {
+            string type = d ->d_name;
+            type = path + type;
+
+            if (stat(type.c_str(), &dst) == 0){
+
+                if( dst.st_mode & S_IFDIR){
+                    type = "Is a folder";
+
+                }
+
+                else if (dst.st_mode & S_IFDIR){
+
+                    type = "Is a file";
+                }
+
+
+
+            }
+
+
+
+
+
+        }
+        cout << d->d_name<<endl<<endl;
+    }
+    closedir(dr);
+
 
 }
 
